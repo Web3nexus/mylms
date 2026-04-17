3a9cf644c6887866cacfc7291b8c0116e9cdb520
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-04-17 03:43:01 -0700

    fix: final branding and dashboard stability fixes

--- frontend/dist/assets/index-RoylUcIr.js
+++ frontend/dist/assets/index-RoylUcIr.js
@@ -1,4 +1,4 @@
-import{a as J,r as b,R as x,f as ne}from"./react-DQKh8YLQ.js";import{j as w}from"./index-ByWZS7uM.js";import{A as oe}from"./index-3kxLbssT.js";var D={exports:{}},I={};/**
+import{a as J,r as b,R as x,f as ne}from"./react-DQKh8YLQ.js";import{j as w}from"./index-5cWRfvnq.js";import{A as oe}from"./index-3kxLbssT.js";var D={exports:{}},I={};/**
  * @license React
  * use-sync-external-store-shim.production.js
  *
